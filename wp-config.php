34b19137bf1dfc93d371762c24cb4b7929b4c987
Arun.n <arun.n@taboola.com>
2021-02-05 18:15:28 +0530

    first commit

--- wp-config.php
+++ wp-config.php
@@ -29,7 +29,7 @@ define( 'DB_USER', 'admin_admin' );
 define( 'DB_PASSWORD', 'dbsolutions' );
 
 /** MySQL hostname */
-define( 'DB_HOST', 'localhost' );
+define( 'DB_HOST', 'database-1.cmldfgqpl9ax.ap-south-1.rds.amazonaws.com' );
 
 /** Database Charset to use in creating database tables. */
 define( 'DB_CHARSET', 'utf8mb4' );
